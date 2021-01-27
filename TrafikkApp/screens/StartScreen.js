35d65e207da6941ea1a8c7da46340604fa662376
Silje Tanemsmo <siljetanemsmo@gmail.com>
2021-01-27 19:00:57 +0100

    Elevated links in StartScreen + clickable (1 link)

--- TrafikkApp/screens/StartScreen.js
+++ TrafikkApp/screens/StartScreen.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import styles from '../styles/mainStyles.js';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import StartScreenLink from '../components/StartScreenLink';
 
 import Colors from '../styles/Colors';
 
@@ -10,92 +10,35 @@ import Colors from '../styles/Colors';
 import Header from '../components/Header.js';
 
 const StartScreen = ({ navigation }) => {
-     return (
-          <SafeAreaView style={styles.container}>
-               <Header name="Home" toggleDrawer={navigation.toggleDrawer} />
-               <View style={screenStyles.container}>
-                    <TouchableOpacity
-                         style={screenStyles.linkContainer}
-                         activeOpacity={0.8}>
-                         <Icon name="times" size={100} color={Colors.light} />
-                         <Text style={screenStyles.linkText}>Veikryss</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                         style={screenStyles.linkContainer}
-                         activeOpacity={0.8}>
-                         <Icon
-                              name="question"
-                              size={100}
-                              color={Colors.light}
-                         />
-                         <Text style={screenStyles.linkText}>tba</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                         style={screenStyles.linkContainer}
-                         activeOpacity={0.8}>
-                         <Icon
-                              name="crosshairs"
-                              size={100}
-                              color={Colors.light}
-                         />
-                         <Text style={screenStyles.linkText}>Rundkjøring</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                         style={screenStyles.linkContainer}
-                         activeOpacity={0.8}>
-                         <Icon
-                              name="question"
-                              size={100}
-                              color={Colors.light}
-                         />
-                         <Text style={screenStyles.linkText}>tba</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                         style={screenStyles.linkContainer}
-                         activeOpacity={0.8}>
-                         <Icon name="road" size={100} color={Colors.light} />
-                         <Text style={screenStyles.linkText}>Vei</Text>
-                    </TouchableOpacity>
-                    <TouchableOpacity
-                         style={screenStyles.linkContainer}
-                         activeOpacity={0.8}>
-                         <Icon
-                              name="question"
-                              size={100}
-                              color={Colors.light}
-                         />
-                         <Text style={screenStyles.linkText}>tba</Text>
-                    </TouchableOpacity>
-               </View>
-          </SafeAreaView>
-     );
+    return (
+        <SafeAreaView style={styles.container}>
+            <Header name="Home" toggleDrawer={navigation.toggleDrawer} />
+            <View style={screenStyles.container}>
+                <StartScreenLink
+                    name="times"
+                    text="Veikryss"
+                    onPress={() => navigation.navigate('IntersectionScreen')}
+                />
+                <StartScreenLink name="question" text="tba" />
+                <StartScreenLink name="crosshairs" text="Rundkjøring" />
+                <StartScreenLink name="question" text="tba" />
+                <StartScreenLink name="road" text="Vei" />
+                <StartScreenLink name="question" text="tba" />
+            </View>
+        </SafeAreaView>
+    );
 };
 
 const screenStyles = StyleSheet.create({
-     container: {
-          flex: 1,
-          flexDirection: 'row',
-          justifyContent: 'center',
-          alignItems: 'center',
-          flexWrap: 'wrap',
-          alignContent: 'center',
-     },
-     linkContainer: {
-          justifyContent: 'center',
-          alignItems: 'center',
-          width: 250,
-          height: 250,
-          borderRadius: 30,
-          borderWidth: 3,
-          borderColor: Colors.light,
-          overflow: 'hidden',
-          margin: 30,
-          backgroundColor: Colors.dark,
-     },
-     linkText: {
-          fontSize: 30,
-          color: Colors.light,
-     },
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+        alignContent: 'center',
+        backgroundColor: Colors.light,
+    },
 });
 
 export default StartScreen;
