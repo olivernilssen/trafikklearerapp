0c953ba0668881eeb683117b4b1347f2f2394ea2
Silje Tanemsmo <siljetanemsmo@gmail.com>
2021-01-27 11:15:17 +0100

    Lagt til Colors-component

---
+++ TrafikkApp/styles/Colors.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet } from 'react-native';
+
+const Colors = StyleSheet.create({
+    primary: {
+        color: '#48466d',
+    },
+    accent: {
+        color: '#48466d',
+    },
+    accent2: {
+        color: '#DDDDDD',
+    },
+});
+
+export default Colors;
