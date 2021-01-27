0645546b5b6d0ca7fef7586822db3966e7f2a158
oliver <oliveren@stud.ntnu.no>
2021-01-27 11:11:16 +0100

    fixed some errors in the code and removed unused imports

--- TrafikkApp/components/DrawerMenu.js
+++ TrafikkApp/components/DrawerMenu.js
@@ -1,49 +1,46 @@
-import React, { Component } from 'react'
-import {
-    StyleSheet,
-    Text,
-    TouchableOpacity,
-    View,
-    FlatList,
-} from 'react-native'
+import React, { Component } from 'react';
+import { Text, TouchableOpacity, View, FlatList } from 'react-native';
 
-import Icon from 'react-native-vector-icons/FontAwesome'
-import drawerStyles from '../styles/drawerStyle.js'
+import Icon from 'react-native-vector-icons/FontAwesome';
+import drawerStyles from '../styles/drawerStyle.js';
 import {
-    DrawerContentScrollView,
-    DrawerItemList,
-} from '@react-navigation/drawer'
+     DrawerContentScrollView,
+     DrawerItemList,
+} from '@react-navigation/drawer';
 
 const menuData = [
-    {
-        icon: 'home',
-        name: 'Hjem',
-        screenName: 'StartScreen',
-        key: 0,
-        value: 0,
-    },
-    {
-        icon: 'sliders',
-        name: 'Innstillinger',
-        screenName: 'SettingsScreen',
-        key: 1,
-        value: 1,
-    },
-    {
-        icon: 'road',
-        name: 'Veikryss',
-        screenName: 'IntersectionScreen',
-        key: 2,
-        value: 2,
-    },
-]
+     {
+          icon: 'home',
+          name: 'Hjem',
+          screenName: 'StartScreen',
+          key: '0',
+          value: 0,
+     },
+     {
+          icon: 'road',
+          name: 'Veikryss',
+          screenName: 'IntersectionScreen',
+          key: '1',
+          value: 1,
+     },
+     {
+          icon: 'sliders',
+          name: 'Innstillinger',
+          screenName: 'SettingsScreen',
+          key: '2',
+          value: 2,
+     },
+];
 
 class DrawerMenu extends Component {
-    render() {
-        return (
-            <View style={drawerStyles.container}>
-                <BackItem navigation={this.props.navigation} icon={'times'} />
-                {/* <DrawerItemList
+     render() {
+          return (
+               <View style={drawerStyles.container}>
+                    <BackItem
+                         navigation={this.props.navigation}
+                         icon={'times'}
+                    />
+                    {/* <DrawerItemList
                     icon={({ name, color, size }) => (
                         <Icon color={color} size={size} name={name} />
                     )}
@@ -51,66 +48,64 @@ class DrawerMenu extends Component {
                     activeTintColor="white"
                     {...this.props}
                 /> */}
-                <FlatList
-                    data={menuData}
-                    renderItem={({ item }) => (
-                        <DrawerItem
-                            navigation={this.props.navigation}
-                            state={this.props.state}
-                            screenName={item.screenName}
-                            icon={item.icon}
-                            name={item.name}
-                            value={item.value}
-                        />
-                    )}
-                />
-            </View>
-        )
-    }
+                    <FlatList
+                         data={menuData}
+                         renderItem={({ item }) => (
+                              <DrawerItem
+                                   navigation={this.props.navigation}
+                                   state={this.props.state}
+                                   screenName={item.screenName}
+                                   icon={item.icon}
+                                   name={item.name}
+                                   value={item.value}
+                                   key={item.key}
+                              />
+                         )}
+                    />
+               </View>
+          );
+     }
 }
 
 const DrawerItem = ({ navigation, state, icon, name, screenName, value }) => (
-    <TouchableOpacity
-        style={drawerStyles.menuItem}
-        activeOpacity={0.2}
-        onPress={() =>
-            navigation.navigate(`${screenName}`, {
-                isStatusBarHidden: false,
-            })
-        }
-    >
-        <Icon
-            name={icon}
-            size={25}
-            color={state.index == value ? '#f05454' : '#DDDDDD'}
-            style={{ margin: 15 }}
-        />
-        <Text
-            style={[
-                drawerStyles.menuItemText,
-                {
-                    color: state.index == value ? '#f05454' : '#DDDDDD',
-                },
-            ]}
-        >
-            {name}
-        </Text>
-    </TouchableOpacity>
-)
+     <TouchableOpacity
+          style={drawerStyles.menuItem}
+          activeOpacity={0.2}
+          onPress={() =>
+               navigation.navigate(`${screenName}`, {
+                    isStatusBarHidden: false,
+               })
+          }>
+          <Icon
+               name={icon}
+               size={25}
+               color={state.index == value ? '#f05454' : '#DDDDDD'}
+               style={{ margin: 15 }}
+          />
+          <Text
+               style={[
+                    drawerStyles.menuItemText,
+                    {
+                         color: state.index == value ? '#f05454' : '#DDDDDD',
+                    },
+               ]}>
+               {name}
+          </Text>
+     </TouchableOpacity>
+);
 
 const BackItem = ({ navigation, icon }) => (
-    <TouchableOpacity
-        style={[{ paddingBottom: 20 }, drawerStyles.menuItem]}
-        activeOpacity={0.2}
-        onPress={() => navigation.toggleDrawer()}
-    >
-        <Icon
-            name={icon}
-            size={35}
-            color="#DDDDDD"
-            style={{ marginTop: 10, marginLeft: 10 }}
-        />
-    </TouchableOpacity>
-)
+     <TouchableOpacity
+          style={[{ paddingBottom: 20 }, drawerStyles.menuItem]}
+          activeOpacity={0.8}
+          onPress={() => navigation.toggleDrawer()}>
+          <Icon
+               name={icon}
+               size={35}
+               color="#DDDDDD"
+               style={{ marginTop: 10, marginLeft: 10 }}
+          />
+     </TouchableOpacity>
+);
 
-export default DrawerMenu
+export default DrawerMenu;
--- TrafikkApp/screens/StartScreen.js
+++ TrafikkApp/screens/StartScreen.js
@@ -1,22 +1,22 @@
-import React from 'react'
-import { View, StyleSheet, Image, Text } from 'react-native'
-import styles from '../styles/mainStyles.js'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import React from 'react';
+import { Image } from 'react-native';
+import styles from '../styles/mainStyles.js';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 // pull in header with DrawerTrigger
-import Header from '../components/Header.js'
+import Header from '../components/Header.js';
 
 const StartScreen = ({ navigation }) => {
-    return (
-        <SafeAreaView style={styles.container}>
-            <Header name="Home" toggleDrawer={navigation.toggleDrawer} />
-            <Image
-                source={require('../assets/banner.png')}
-                style={{ width: '100%', height: '90%' }}
-                resizeMode="contain"
-            />
-        </SafeAreaView>
-    )
-}
+     return (
+          <SafeAreaView style={styles.container}>
+               <Header name="Home" toggleDrawer={navigation.toggleDrawer} />
+               <Image
+                    source={require('../assets/banner.png')}
+                    style={{ width: '100%', height: '90%' }}
+                    resizeMode="contain"
+               />
+          </SafeAreaView>
+     );
+};
 
-export default StartScreen
+export default StartScreen;
