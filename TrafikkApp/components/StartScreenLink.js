0ff868ee50a51ddd8eb0b6ac8873505dcbbf3c82
Silje Tanemsmo <siljetanemsmo@gmail.com>
2021-01-29 13:21:42 +0100

    La til RComponent for responsive ikoner på StartScreen

--- TrafikkApp/components/StartScreenLink.js
+++ TrafikkApp/components/StartScreenLink.js
@@ -1,37 +1,64 @@
 import React from 'react';
-import { Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
-
 import Colors from '../styles/Colors';
+import RComponent, { RUtils } from 'react-native-responsive-component';
+
+const textSize = RUtils.isSmallScreen() ? 20 : 30;
 
 const StartScreenLink = (props) => {
+    const iconSize = RUtils.isSmallScreen() ? 50 : 100;
+
     return (
-        <TouchableOpacity
-            style={styles.linkContainer}
-            activeOpacity={0.6}
-            onPress={props.onPress}>
-            <Icon name={props.name} size={100} color={Colors.light} />
-            <Text style={styles.linkText}>{props.text}</Text>
-        </TouchableOpacity>
+        <RComponent
+            render$={() => View}
+            style$sm={styles.containerSm}
+            style$md={styles.containerMd}>
+            <TouchableOpacity
+                style={styles.linkButton}
+                activeOpacity={0.6}
+                onPress={props.onPress}>
+                <Icon name={props.name} size={iconSize} color={Colors.light} />
+                <Text style={styles.linkText}>{props.text}</Text>
+            </TouchableOpacity>
+        </RComponent>
     );
 };
 
 const styles = StyleSheet.create({
-    linkContainer: {
+    containerMd: {
         justifyContent: 'center',
         alignItems: 'center',
-        width: 200,
-        height: 200,
+        width: 250,
+        height: 250,
         borderRadius: 25,
         borderWidth: 2,
         borderColor: Colors.light,
-        margin: 40,
+        margin: 30,
         backgroundColor: Colors.colorful,
         elevation: 10,
     },
+    containerSm: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 130,
+        height: 130,
+        borderRadius: 25,
+        borderWidth: 2,
+        borderColor: Colors.light,
+        margin: 20,
+        backgroundColor: Colors.colorful,
+        elevation: 10,
+    },
+    linkButton: {
+        height: '100%',
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
     linkText: {
         marginTop: 15,
-        fontSize: 25,
+        fontSize: textSize,
         color: Colors.light,
     },
 });
