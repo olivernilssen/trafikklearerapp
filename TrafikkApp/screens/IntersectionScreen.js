f1c01a0502dd485e08359e0696633ae17a0cd074
joakiht@ntnu.no <joakiht@ntnu.no>
2021-01-30 00:04:40 +0100

    added background to IntersectionScreen

--- TrafikkApp/screens/IntersectionScreen.js
+++ TrafikkApp/screens/IntersectionScreen.js
@@ -1,16 +1,21 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styles from '../styles/mainStyles.js';
 import Header from '../components/Header.js';
+
 
 const IntersectionScreen = ({ navigation }) => {
     return (
         <SafeAreaView style={styles.container}>
             <Header name="Veikryss" navigation={navigation} />
             <View style={screenStyles.main}>
-                <Text>Veikryss siden</Text>
+                <ImageBackground style={screenStyles.backgroundImage}
+                source={require('../assets/temp_kryss.png')}
+                >
+                    <Text>Veikryss siden</Text>
+                </ImageBackground>
             </View>
         </SafeAreaView>
     );
@@ -18,10 +23,17 @@ const IntersectionScreen = ({ navigation }) => {
 
 const screenStyles = StyleSheet.create({
     main: {
+        flex: 1,
         height: '90%',
+        width: '100%',
         justifyContent: 'center',
         alignItems: 'center',
     },
+    backgroundImage: {
+        width: '100%',
+        height: '100%',
+    }
 });
+
 
 export default IntersectionScreen;
