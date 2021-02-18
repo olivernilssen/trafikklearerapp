ac2b30113edb2a3536b10834b00ce7f6f4fd0939
oliver <oliveren@stud.ntnu.no>
2021-02-18 15:04:20 +0100

    had to add menuprovider around the navigator in app.js because it was gone

--- TrafikkApp/App.js
+++ TrafikkApp/App.js
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 
+import { MenuProvider } from 'react-native-popup-menu';
+
 import Navigator from './components/drawerComponents/Navigator';
 
 export default function App() {
     return (
-        <NavigationContainer>
-            <Navigator />
-        </NavigationContainer>
+        <MenuProvider>
+            <NavigationContainer>
+                <Navigator />
+            </NavigationContainer>
+        </MenuProvider>
     );
 }
