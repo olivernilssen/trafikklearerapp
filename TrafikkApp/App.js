f7f2a3f512b15bccab2e7f037c80a36c2730903b
oliver <oliveren@stud.ntnu.no>
2021-02-18 13:14:05 +0100

    small changes

--- TrafikkApp/App.js
+++ TrafikkApp/App.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-import { MenuProvider } from 'react-native-popup-menu';
 
 import Navigator from './components/drawerComponents/Navigator';
 
