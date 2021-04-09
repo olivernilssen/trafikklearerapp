8af13fe55963345c75544410794163aed11cc8c5
oliver <oliveren@stud.ntnu.no>
2021-04-09 13:57:08 +0200

    fixed some names and added some index files for jsdoc

---
+++ TrafikkApp/components/draggable/index.js
@@ -0,0 +1,7 @@
+export { default as Draggable } from './Draggable';
+export { default as DraggableWithEverything } from './DraggableWithEverything';
+export { default as MappingDraggables } from './MappingDraggables';
+
+/** The draggable components
+ * @namespace draggable
+ */
