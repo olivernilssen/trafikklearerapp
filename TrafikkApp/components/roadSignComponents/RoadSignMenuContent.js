6e5304d373d945987acef969cb9eb1efe1e88c0c
joakiht@ntnu.no <joakiht@ntnu.no>
2021-04-28 00:47:12 +0200

    bigger buttons in bottomMenu, RoadSign

--- TrafikkApp/components/roadSignComponents/RoadSignMenuContent.js
+++ TrafikkApp/components/roadSignComponents/RoadSignMenuContent.js
@@ -90,10 +90,11 @@ const RoadSignMenuContent = (props) => {
             <View
                 key={index}
                 style={{
-                    width: '30%',
+                    width: '32%',
                     height: '30%',
                     alignItems: 'center',
-                    paddingVertical: 10,
+                    // paddingVertical: 10,
+                    paddingTop: '1%',
                 }}>
                 <TouchableOpacity
                     onPress={() => {
@@ -131,7 +132,7 @@ const RoadSignMenuContent = (props) => {
                 flexDirection: 'row',
                 flexWrap: 'wrap',
                 justifyContent: 'space-around',
-                padding: 10,
+                // paddingTop: '2%',
             }}>
             {SignTypeButton}
         </View>
@@ -150,7 +151,7 @@ const styles = StyleSheet.create({
     textStyle: {
         textAlign: 'center',
         // fontSize: 20,
-        padding: 10,
+        padding: '7%',
         ...Typography.button,
     },
     button: {
