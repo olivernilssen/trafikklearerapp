20a7592952e26f1cd9fd17bfe955d907302c9895
oliver <oliveren@stud.ntnu.no>
2021-05-05 15:32:43 +0200

    merge

---
+++ TrafikkApp/assets/sign_descriptions/fareskiltData.js
@@ -0,0 +1,305 @@
+const fareskiltData = {
+    '100_1': {
+        navn: 'Farlig sving',
+        beskrivelse: 'til høyre',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/100_1.png'),
+        source: require('../roadSigns/Fareskilt/100_1.png'),
+    },
+    '100_2': {
+        navn: 'Farlig sving',
+        beskrivelse: 'til venstre',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/100_2.png'),
+        source: require('../roadSigns/Fareskilt/100_2.png'),
+    },
+    '102_1': {
+        navn: 'Farlige svinger',
+        beskrivelse: 'den første til høyre',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/102_1.png'),
+        source: require('../roadSigns/Fareskilt/102_1.png'),
+    },
+    '102_2': {
+        navn: 'Farlige svinger',
+        beskrivelse: 'den første til venstre',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/102_2.png'),
+        source: require('../roadSigns/Fareskilt/102_2.png'),
+    },
+    '104_1': {
+        navn: 'Bratt bakke',
+        beskrivelse: 'stigning',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/104_1.png'),
+        source: require('../roadSigns/Fareskilt/104_1.png'),
+    },
+    '104_2': {
+        navn: 'Bratt bakke',
+        beskrivelse: 'fall',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/104_2.png'),
+        source: require('../roadSigns/Fareskilt/104_2.png'),
+    },
+    '106_1': {
+        navn: 'Smalere veg',
+        beskrivelse: 'innsnevring på begge sider',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/106_1.png'),
+        source: require('../roadSigns/Fareskilt/106_1.png'),
+    },
+    '106_2': {
+        navn: 'Smalere veg',
+        beskrivelse: 'innsnevring på høyre side',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/106_2.png'),
+        source: require('../roadSigns/Fareskilt/106_2.png'),
+    },
+    '106_3': {
+        navn: 'Smalere veg',
+        beskrivelse: 'innsnevring på venstre side',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/106_3.png'),
+        source: require('../roadSigns/Fareskilt/106_3.png'),
+    },
+    '108_0': {
+        navn: 'Ujevn veg',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/108_0.png'),
+        source: require('../roadSigns/Fareskilt/108_0.png'),
+    },
+    '109_0': {
+        navn: 'Fartshump',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/109_0.png'),
+        source: require('../roadSigns/Fareskilt/109_0.png'),
+    },
+    '110_0': {
+        navn: 'Vegarbeid',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/110_0.png'),
+        source: require('../roadSigns/Fareskilt/110_0.png'),
+    },
+    '112_0': {
+        navn: 'Steinsprut',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/112_0.png'),
+        source: require('../roadSigns/Fareskilt/112_0.png'),
+    },
+    '114_1': {
+        navn: 'Rasfare',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/114_1.png'),
+        source: require('../roadSigns/Fareskilt/114_1.png'),
+    },
+    '116_0': {
+        navn: 'Glatt kjørebane',
+        beskrivelse:
+            'Skiltet varsler om fare for glatt kjørebane på grunn av særlige forhold.',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/116_0.png'),
+        source: require('../roadSigns/Fareskilt/116_0.png'),
+    },
+    '117_0': {
+        navn: 'Farlig vegskulder',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/117_0.png'),
+        source: require('../roadSigns/Fareskilt/117_0.png'),
+    },
+    '118_0': {
+        navn: 'Bevegelig bru',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/118_0.png'),
+        source: require('../roadSigns/Fareskilt/118_0.png'),
+    },
+    '120_0': {
+        navn: 'Kai, strand eller ferjeleie',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/120_0.png'),
+        source: require('../roadSigns/Fareskilt/120_0.png'),
+    },
+    '122_0': {
+        navn: 'Tunnel',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/122_0.png'),
+        source: require('../roadSigns/Fareskilt/122_0.png'),
+    },
+    '124_0': {
+        navn: 'Farlig vegkryss',
+        beskrivelse:
+            'Skiltet varsler om farlig veikryss hvor det gjelder vanlig vikeplikt overfor kjørende fra høyre. \r',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/124_0.png'),
+        source: require('../roadSigns/Fareskilt/124_0.png'),
+    },
+    '126_0': {
+        navn: 'Rundkjøring',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/126_0.png'),
+        source: require('../roadSigns/Fareskilt/126_0.png'),
+    },
+    '132_0': {
+        navn: 'Trafikklyssignal',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/132_0.png'),
+        source: require('../roadSigns/Fareskilt/132_0.png'),
+    },
+    '134_0': {
+        navn: 'Planovergang med bom',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/134_0.png'),
+        source: require('../roadSigns/Fareskilt/134_0.png'),
+    },
+    '135_0': {
+        navn: '(134.2) Planovergang uten bom',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/135_0.png'),
+        source: require('../roadSigns/Fareskilt/135_0.png'),
+    },
+    '136_1': {
+        navn: 'Avstandsskilt',
+        beskrivelse:
+            'Skiltet gir forvarsel om planovergang varslet med skilt 134 eller 135',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/136_1.png'),
+        source: require('../roadSigns/Fareskilt/136_1.png'),
+    },
+    '136_2': {
+        navn: 'Avstandsskilt',
+        beskrivelse:
+            'Skiltet gir forvarsel om planovergang varslet med skilt 134 eller 135',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/136_2.png'),
+        source: require('../roadSigns/Fareskilt/136_2.png'),
+    },
+    '136_3': {
+        navn: 'Avstandsskilt',
+        beskrivelse:
+            'Skiltet gir forvarsel om planovergang varslet med skilt 134 eller 135',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/136_3.png'),
+        source: require('../roadSigns/Fareskilt/136_3.png'),
+    },
+    '138_1': {
+        navn: 'Jernbanespor',
+        beskrivelse:
+            'Enkeltsporet. Skiltet angir stedet hvor jernbane eller forstadsbane krysser veien i plan.',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/138_1.png'),
+        source: require('../roadSigns/Fareskilt/138_1.png'),
+    },
+    '138_2': {
+        navn: 'Jernbanespor',
+        beskrivelse:
+            'Flersporet. Skiltet angir stedet hvor jernbane eller forstadsbane krysser veien i plan.',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/138_2.png'),
+        source: require('../roadSigns/Fareskilt/138_2.png'),
+    },
+    '139_0': {
+        navn: 'Sporvogn',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/139_0.png'),
+        source: require('../roadSigns/Fareskilt/139_0.png'),
+    },
+    '140_0': {
+        navn: 'Avstand til gangfelt',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/140_0.png'),
+        source: require('../roadSigns/Fareskilt/140_0.png'),
+    },
+    '142_0': {
+        navn: 'Barn ',
+        beskrivelse:
+            'Skiltet varsler om sted på eller langs vei hvor barn ofte ferdes eller oppholder seg i forbindelse med skole, barnehage, lekeplass, eller lignende.',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/142_0.png'),
+        source: require('../roadSigns/Fareskilt/142_0.png'),
+    },
+    '144_0': {
+        navn: 'Syklende',
+        beskrivelse:
+            'Skiltet varsler om sted hvor syklende ofte krysser eller kjører ut på veien.',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/144_0.png'),
+        source: require('../roadSigns/Fareskilt/144_0.png'),
+    },
+    '146_1': {
+        navn: 'Dyr',
+        beskrivelse:
+            'Skiltet varsler at elg ofte ferdes over eller langs veien.',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/146_1.png'),
+        source: require('../roadSigns/Fareskilt/146_1.png'),
+    },
+    '146_2': {
+        navn: 'Dyr',
+        beskrivelse:
+            'Skiltet varsler at rein ofte ferdes over eller langs veien.',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/146_2.png'),
+        source: require('../roadSigns/Fareskilt/146_2.png'),
+    },
+    '146_3': {
+        navn: 'Dyr',
+        beskrivelse:
+            'Skiltet varsler at hjort ofte ferdes over eller langs veien.',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/146_3.png'),
+        source: require('../roadSigns/Fareskilt/146_3.png'),
+    },
+    '146_4': {
+        navn: 'Dyr',
+        beskrivelse:
+            'Skiltet varsler at kyr ofte ferdes over eller langs veien.',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/146_4.png'),
+        source: require('../roadSigns/Fareskilt/146_4.png'),
+    },
+    '146_5': {
+        navn: 'Dyr',
+        beskrivelse:
+            'Skiltet varsler at sauer ofte ferdes over eller langs veien.',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/146_5.png'),
+        source: require('../roadSigns/Fareskilt/146_5.png'),
+    },
+    '148_0': {
+        navn: 'Møtende trafikk',
+        beskrivelse:
+            'Skiltet varsler at kjørebanen har kjørende trafikk i begge retninger.',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/148_0.png'),
+        source: require('../roadSigns/Fareskilt/148_0.png'),
+    },
+    '149_0': {
+        navn: 'Kø',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/149_0.png'),
+        source: require('../roadSigns/Fareskilt/149_0.png'),
+    },
+    '150_0': {
+        navn: 'Fly',
+        beskrivelse:
+            'Skiltet varsler at fly kan fly lavt over eller nær veien.',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/150_0.png'),
+        source: require('../roadSigns/Fareskilt/150_0.png'),
+    },
+    '151_0': {
+        navn: 'Militær aktivitet',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/151_0.png'),
+        source: require('../roadSigns/Fareskilt/151_0.png'),
+    },
+    '152_0': {
+        navn: 'Sidevind',
+        beskrivelse:
+            'Skiltet varsler om sted hvor det ofte forekommer sterk sidevind.',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/152_0.png'),
+        source: require('../roadSigns/Fareskilt/152_0.png'),
+    },
+    '153_0': {
+        navn: 'Trafikkulykke',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/153_0.png'),
+        source: require('../roadSigns/Fareskilt/153_0.png'),
+    },
+    '154_0': {
+        navn: 'Skiløpere ',
+        beskrivelse:
+            'Skiltet varsler om sted hvor skiløpere ofte krysser veien.',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/154_0.png'),
+        source: require('../roadSigns/Fareskilt/154_0.png'),
+    },
+    '155_0': {
+        navn: 'Ridende',
+        beskrivelse:
+            'Skiltet varsler om sted hvor ridende ofte krysser eller rir ut i veien.',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/155_0.png'),
+        source: require('../roadSigns/Fareskilt/155_0.png'),
+    },
+    '156_0': {
+        navn: 'Annen fare',
+        beskrivelse: 'Farens art er angitt på underskilt.',
+        thumbnail: require('../roadSigns/Fareskilt/thumbnails/156_0.png'),
+        source: require('../roadSigns/Fareskilt/156_0.png'),
+    },
+};
+
+export default fareskiltData;
---
+++ TrafikkApp/assets/sign_descriptions/markeringsskiltData.js
@@ -0,0 +1,110 @@
+const markeringsskiltData = {
+    '902H_0': {
+        navn: 'Bakgrunnsmarkering',
+        beskrivelse: '902H',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/902_0H.png'),
+        source: require('../roadSigns/Markeringsskilt/902_0H.png'),
+    },
+    '902V_0': {
+        navn: 'Bakgrunnsmarkering',
+        beskrivelse: '902V',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/902_0V.png'),
+        source: require('../roadSigns/Markeringsskilt/902_0V.png'),
+    },
+    '904H_0': {
+        navn: 'Retningsmarkering',
+        beskrivelse: '904H',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/904_0H.png'),
+        source: require('../roadSigns/Markeringsskilt/904_0H.png'),
+    },
+    '904V_0': {
+        navn: 'Retningsmarkering',
+        beskrivelse: '904V',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/904_0V.png'),
+        source: require('../roadSigns/Markeringsskilt/904_0V.png'),
+    },
+    '906H_0': {
+        navn: 'Hindermarkering',
+        beskrivelse:
+            'Skråstripene peker ned mot den side markeringen kan passeres på',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/906_0H.png'),
+        source: require('../roadSigns/Markeringsskilt/906_0H.png'),
+    },
+    '906V_0': {
+        navn: 'Hindermarkering',
+        beskrivelse:
+            'Skråstripene peker ned mot den side markeringen kan passeres på',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/906_0V.png'),
+        source: require('../roadSigns/Markeringsskilt/906_0V.png'),
+    },
+    '906VH_0': {
+        navn: 'Hindermarkering',
+        beskrivelse:
+            'Skråstripene peker ned mot den side markeringen kan passeres på',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/906_0VH.png'),
+        source: require('../roadSigns/Markeringsskilt/906_0VH.png'),
+    },
+    '908_0': {
+        navn: 'Hindermarkering',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/908_0.png'),
+        source: require('../roadSigns/Markeringsskilt/908_0.png'),
+    },
+    '912_0': {
+        navn: 'Avkjøringsmarkering',
+        beskrivelse:
+            'Skiltet markerer avkjøringsvei fra motorvei eller annen vei med høy standard.',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/912_0.png'),
+        source: require('../roadSigns/Markeringsskilt/912_0.png'),
+    },
+    '914V_0': {
+        navn: 'Tunnelmarkering',
+        beskrivelse: '914V',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/914_V.png'),
+        source: require('../roadSigns/Markeringsskilt/914_V.png'),
+    },
+    '914H_0': {
+        navn: 'Tunnelmarkering',
+        beskrivelse: '914H',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/914_H.png'),
+        source: require('../roadSigns/Markeringsskilt/914_H.png'),
+    },
+    '916_0': {
+        navn: 'Avstandsmerking i tunnel',
+        beskrivelse: 'Skiltet markerer avstand til tunnelåpninger.',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/916_0.png'),
+        source: require('../roadSigns/Markeringsskilt/916_0.png'),
+    },
+    '920VM_0': {
+        navn: 'Kantstolpe',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/920_VM.png'),
+        source: require('../roadSigns/Markeringsskilt/920_VM.png'),
+    },
+    '920H_0': {
+        navn: 'Kantstolpe',
+        beskrivelse: '920H',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/920_H.png'),
+        source: require('../roadSigns/Markeringsskilt/920_H.png'),
+    },
+    '920VE_0': {
+        navn: 'Kantstolpe',
+        beskrivelse: '920VE',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/920_VE.png'),
+        source: require('../roadSigns/Markeringsskilt/920_VE.png'),
+    },
+    '940_0': {
+        navn: 'Trafikkjegle',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/940.png'),
+        source: require('../roadSigns/Markeringsskilt/940.png'),
+    },
+    '942_0': {
+        navn: 'Trafikksylinder',
+        beskrivelse: '',
+        thumbnail: require('../roadSigns/Markeringsskilt/thumbnails/942.png'),
+        source: require('../roadSigns/Markeringsskilt/942.png'),
+    },
+};
+
+export default markeringsskiltData;
